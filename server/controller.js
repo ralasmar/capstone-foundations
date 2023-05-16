1389d46e97202a481758c51d7bb07403ca7e8475
Rola Alasmar <alasm002@umn.edu>
2023-05-16 11:52:56 -0500

    capstone

---
+++ server/controller.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const Sequelize = require('sequelize')
+const { CONNECTION_STRING } = process.env
+
+//initialize new Sequelize instance passing connection string and object
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+
+module.exports = {
+    
+}
---
+++ server/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require("express")
+const cors = require("cors")
+const app = express();
+const {SERVER_PORT} = process.env
+
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(SERVER_PORT, () => console.log(`server running on ${SERVER_PORT}`))
